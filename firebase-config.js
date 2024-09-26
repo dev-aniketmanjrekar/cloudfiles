5cba151961fa1dd27f9a9420b026e3c80ee1931f
Aniket Manjrekar <120080012+dev-aniketmanjrekar@users.noreply.github.com>
2024-09-26 18:20:16 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDumDjFmK4XnzPfvtq0Kw6tXb_CM2qxQmQ",
+    authDomain: "cloudfiles-41988.firebaseapp.com",
+    projectId: "cloudfiles-41988",
+    storageBucket: "cloudfiles-41988.appspot.com",
+    messagingSenderId: "864411341034",
+    appId: "1:864411341034:web:30e63e9f19f68f5a268793",
+    measurementId: "G-31YFWX5F6S" // Optional
+};
